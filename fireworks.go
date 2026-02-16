9b6ca15b59737bc098652f0fcba9929cf5a128a2
Rem-moe <abhaykv111@gmail.com>
2026-02-16 21:32:43 +0530

    feat: enhance Firework and FireworkRule structs with color assignment and random color generation

--- fireworks.go
+++ fireworks.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"math/rand"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 type Firework struct {
@@ -10,12 +12,13 @@ type Firework struct {
 	Type int
 
 	Age float64
+
+	Color rl.Color
 }
 
 type Payload struct {
 	Type  int
 	Count int
-	Color float64
 }
 
 type FireworkRule struct {
@@ -33,9 +36,10 @@ type FireworkRule struct {
 }
 
 func (f *Firework) Update(duration float64) bool {
-	f.Integrate(duration) // sets position and velocity
+	f.Integrate(duration)
 
-	f.Age -= 1
+	// FIX 1: Subtract the time passed (0.016), not 1.0!
+	f.Age -= duration
 
 	return f.Age < 0 || f.Position.Y < 0
 }
@@ -62,6 +66,12 @@ func (rule *FireworkRule) Create(f *Firework, parent *Firework) {
 	f.Accelaration = Vector3D{X: 0, Y: -9.8, Z: 0} // Gravity
 
 	f.ClearAccumulator()
+
+	if f.Type == 1 {
+		f.Color = rl.Red // Rockets are Red
+	} else {
+		f.Color = RandomColor() // Sparks are random
+	}
 }
 
 func randomRange(min, max float64) float64 {
@@ -75,4 +85,12 @@ func randomVector(min, max Vector3D) Vector3D {
 		Y: randomRange(min.Y, max.Y),
 		Z: randomRange(min.Z, max.Z),
 	}
+}
+func RandomColor() rl.Color {
+	return rl.NewColor(
+		uint8(rand.Intn(256)), // Red (0-255)
+		uint8(rand.Intn(256)), // Green
+		uint8(rand.Intn(256)), // Blue
+		255,                   // Alpha (255 = Opaque)
+	)
 }
