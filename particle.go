e8ac17a00481086a447934ee8fcf6571c586e17f
REM-moe <abhaykv111@gmail.com>
2026-02-16 15:37:01 +0530

    feat: add Particle struct to represent physical particles in the engine

---
+++ particle.go
@@ -0,0 +1,14 @@
+package gengine
+
+type Particle struct {
+	Position Vector3D
+
+	Velocity Vector3D
+
+	Accelaration Vector3D
+
+	// this is friction or drag that a particle needs
+	Damping float64
+
+	InverseMass float64
+}
